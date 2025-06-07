c555476fc929d78e9ec539106fb7a2808eddbd01
ArpanBagadia <arpanbagadia@gmail.com>
2025-06-07 10:22:13 +0530

    all changer

---
+++ careerMap/backend/controllers/courseControllers.js
@@ -0,0 +1,100 @@
+const Course = require('../models/Course')
+
+exports.courseUpload = async (req, res) => {
+    try {
+        const { title, subtitle, category, description, level, price, tutorId } = req.body;
+        const imageUrl = req.files['image']?.[0]?.path;
+        const videoUrl = req.files['video']?.[0]?.path;
+        const newCourse = new Course({
+            title,
+            subtitle,
+            category,
+            description,
+            level,
+            price,
+            imageUrl,
+            videoUrl,
+            tutorId
+        })
+        await newCourse.save();
+        res.status(201).json({
+            success: true,
+            course: newCourse
+        })
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            msg: "upload failed"
+        })
+    }
+}
+
+exports.getTutorCourses = async (req, res) => {
+    const { tutorId } = req.query;
+    try {
+        if (!tutorId) {
+            return res.status(400).json({
+                success: false,
+                msg: "Tutor ID is required"
+            })
+        }
+        const courses = await Course.find({ tutorId })
+        res.status(200).json({
+            success: true,
+            count: courses.length,
+            courses
+        });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            msg: "Failed to fetch courses"
+        })
+    }
+}
+
+exports.updateCourse = async (req, res) => {
+    const { id } = req.query;
+    const {
+        title, subtitle, category, description,
+        level, price, tutorId
+    } = req.body;
+
+    try {
+        const existingCourse = await Course.findOne({ tutorId });
+        console.log(id)
+        console.log(existingCourse.tutorId)
+        if (!existingCourse) {
+            return res.status(404).json({ success: false, msg: "Course not found" });
+        }
+
+        if (existingCourse.tutorId.toString() !== tutorId.toString()) {
+            return res.status(403).json({ success: false, msg: "Unauthorized access" });
+        }
+
+        existingCourse.title = title || existingCourse.title;
+        existingCourse.subtitle = subtitle || existingCourse.subtitle;
+        existingCourse.category = category || existingCourse.category;
+        existingCourse.description = description || existingCourse.description;
+        existingCourse.level = level || existingCourse.level;
+        existingCourse.price = price || existingCourse.price;
+
+        if (req.files['image']) {
+            existingCourse.imageUrl = req.files['image'][0].path;
+        }
+        if (req.files['video']) {
+            existingCourse.videoUrl = req.files['video'][0].path;
+        }
+
+        await existingCourse.save();
+
+        res.status(200).json({ success: true, course: existingCourse });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false, msg: "Update failed" });
+    }
+};
---
+++ careerMap/backend/utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudeinary = require('cloudinary').v2;
+
+cloudeinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+module.exports = cloudeinary;
