96a087c91e5d74193a14a641c0319d162276b36f
ArpanBagadia <arpanbagadia@gmail.com>
2025-06-16 14:25:35 +0530

    category

--- careerMap/backend/models/Course.js
+++ careerMap/backend/models/Course.js
@@ -14,6 +14,10 @@ const courseSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    category:{
+        type:String,
+        required:true
+    },
     description: {
         type: String,
         required: true
